9e5795cd9f4dc3b8914c0aac04ce56b32c454135
BunnyHare <38251635+BunnyHare@users.noreply.github.com>
2019-03-04 18:17:14 +0800

    Add files via upload

--- GetCaptchaImages.py
+++ GetCaptchaImages.py
@@ -1,5 +1,5 @@
-import requests
 import urllib
+import urllib.request
 import re
 import os
 
@@ -26,7 +26,7 @@ def getHtml(url):
     return html
 
 def getImg(html):   # 爬不同网页要修改这里的str
-    str = '<img id="loginform-verifycode-image" src="(.*)" alt'
+    str = '<img id="loginform-verifycode-image" src="(.*)" alt' # 西电信息化建设处
     imgre=re.compile(str)
     imglist=imgre.findall(html)
     return imglist
@@ -39,16 +39,23 @@ def make_dir(folder):
 
 def save_img(url,path,imglist,filename):
     img_url=url+imglist[0]
-    # filename=path+'\\'+'test.jpg'
     img=urllib.request.urlretrieve(img_url,filename)
 
 
 if __name__=='__main__':
-    path=make_dir('CaptchaImg')
-    url='https://pay.xidian.edu.cn/'
-    for i in range(100):
-        html=getHtml(url)
-        imglist=getImg(html)
-        filename=path+'\\'+str(i+1)+'.jpg'
-        save_img(url,path,imglist,filename)
+    # '''西电信息化建设处'''
+    # path=make_dir('CaptchaImg_xidian')
+    # url='https://pay.xidian.edu.cn/'
+    # for i in range(5):
+    #     html=getHtml(url)
+    #     imglist=getImg(html)
+    #     filename=path+'\\'+str(i+1)+'.jpg'
+    #     print('正在下载第'+str(i+1)+'张图片')
+    #     save_img(url,path,imglist,filename)
 
+    # '''饭否'''
+    path = make_dir('CaptchaImg_fanfou')
+    for i in range(100):
+        filename = path + '\\' + str(i + 1) + '.jpg'
+        print('正在下载第' + str(i + 1) + '张图片')
+        urllib.request.urlretrieve('http://captcha.fanfou.com/captcha.png', filename)
